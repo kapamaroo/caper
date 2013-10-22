1e48976a728e4002615144a8b2f2942cd24a4949
Maroudas Manolis <kapamaroo@gmail.com>
2013-10-22 03:15:46 +0300

    add helper method to main.c file

--- main.c
+++ main.c
@@ -11,12 +11,22 @@ void about() {
 
 }
 
+void handle_file(char *filename) {
+    int status = parse_file(filename);
+    print_elements();
+    print_nodes();
+    if (status == SEMANTIC_ERRORS) {
+        printf("\nError: input file '%s' is not well defined - exit.\n\n",filename);
+        exit(EXIT_FAILURE);
+    }
+}
+
 int main(int argc, char *argv[]) {
     int i;
 
     parser_init();
     for (i=1; i<argc; ++i) {
-        parse_file(argv[i]);
+        handle_file(argv[i]);
     }
 
     printf("\nTerminating...\n\n");
